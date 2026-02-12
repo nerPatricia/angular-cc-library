863aca24a6296bf64a986d0e1117ea8c8b341608
patricia.costa <patricia.costa@telefonicabigdata.onmicrosoft.com>
2026-02-12 12:29:14 -0300

    refactor(tests): update fakeasync to async and prettier formating

--- projects/angular-cc-library/src/lib/directives/credit-card-format.directive.spec.ts
+++ projects/angular-cc-library/src/lib/directives/credit-card-format.directive.spec.ts
@@ -1,8 +1,8 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { CreditCardFormatDirective } from './credit-card-format.directive';
-import { Component, DebugElement } from '@angular/core';
-import { By } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CreditCardFormatDirective } from "./credit-card-format.directive";
+import { Component, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
 
 const KEY_MAP = {
   ONE: 49, // input `1`
@@ -10,19 +10,21 @@ const KEY_MAP = {
 };
 
 function createKeyEvent(keyCode: number) {
-  return {keyCode, which: keyCode, preventDefault: jest.fn()};
+  return { keyCode, which: keyCode, preventDefault: jest.fn() };
 }
 
-function triggerKeyEvent(input: DebugElement, eventName: string, keyCode: number) {
+function triggerKeyEvent(
+  input: DebugElement,
+  eventName: string,
+  keyCode: number,
+) {
   input.triggerEventHandler(eventName, createKeyEvent(keyCode));
 }
 
-
-describe('Directive: CreditCardFormat', () => {
-
-  describe('general cases', () => {
+describe("Directive: CreditCardFormat", () => {
+  describe("general cases", () => {
     @Component({
-      template: `<input type="tel" ccNumber>`,
+      template: `<input type="tel" ccNumber />`,
       imports: [CreditCardFormatDirective],
     })
     class TestCreditCardFormatComponent {}
@@ -31,121 +33,112 @@ describe('Directive: CreditCardFormat', () => {
     let inputEl: DebugElement;
     beforeEach(() => {
       TestBed.configureTestingModule({
-        imports: [TestCreditCardFormatComponent]
+        imports: [TestCreditCardFormatComponent],
       });
       fixture = TestBed.createComponent(TestCreditCardFormatComponent);
-      inputEl = fixture.debugElement.query(By.css('input'));
+      inputEl = fixture.debugElement.query(By.css("input"));
     });
 
-    it('formats card number tick by tick', fakeAsync(() => {
-
-      inputEl.nativeElement.value = '4111 1111';
-      triggerKeyEvent(inputEl, 'keydown', KEY_MAP.ONE);
+    it("formats card number tick by tick", async () => {
+      inputEl.nativeElement.value = "4111 1111";
+      triggerKeyEvent(inputEl, "keydown", KEY_MAP.ONE);
 
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111');
+      expect(inputEl.nativeElement.value).toBe("4111 1111");
 
-      triggerKeyEvent(inputEl, 'keypress', KEY_MAP.ONE);
+      triggerKeyEvent(inputEl, "keypress", KEY_MAP.ONE);
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111');
+      expect(inputEl.nativeElement.value).toBe("4111 1111");
 
       // the value is changed here by the browser as default behavior
-      inputEl.nativeElement.value = '4111 11111';
+      inputEl.nativeElement.value = "4111 11111";
 
       inputEl.nativeElement.focus();
-      inputEl.triggerEventHandler('input', null);
+      inputEl.triggerEventHandler("input", null);
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111 1');
-      triggerKeyEvent(inputEl, 'keyup', KEY_MAP.ONE);
+      expect(inputEl.nativeElement.value).toBe("4111 1111 1");
+      triggerKeyEvent(inputEl, "keyup", KEY_MAP.ONE);
 
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111 1');
-    }));
+      expect(inputEl.nativeElement.value).toBe("4111 1111 1");
+    });
 
-    it('formats card number one tick', fakeAsync(() => {
-
-      inputEl.nativeElement.value = '4111 1111';
-      inputEl.triggerEventHandler('keydown', {keyCode: KEY_MAP.ONE, which: KEY_MAP.ONE});
+    it("formats card number one tick", async () => {
+      inputEl.nativeElement.value = "4111 1111";
+      inputEl.triggerEventHandler("keydown", {
+        keyCode: KEY_MAP.ONE,
+        which: KEY_MAP.ONE,
+      });
       fixture.detectChanges();
-      expect(inputEl.nativeElement.value).toBe('4111 1111');
+      expect(inputEl.nativeElement.value).toBe("4111 1111");
 
-      inputEl.triggerEventHandler('keypress', {keyCode: KEY_MAP.ONE, which: KEY_MAP.ONE});
+      inputEl.triggerEventHandler("keypress", {
+        keyCode: KEY_MAP.ONE,
+        which: KEY_MAP.ONE,
+      });
       fixture.detectChanges();
-      expect(inputEl.nativeElement.value).toBe('4111 1111');
+      expect(inputEl.nativeElement.value).toBe("4111 1111");
 
       // the value is changed here by the browser as default behavior
-      inputEl.nativeElement.value = '4111 11111';
+      inputEl.nativeElement.value = "4111 11111";
 
-      inputEl.triggerEventHandler('input', null);
+      inputEl.triggerEventHandler("input", null);
       fixture.detectChanges();
-      expect(inputEl.nativeElement.value).toBe('4111 1111 1');
-    }));
+      expect(inputEl.nativeElement.value).toBe("4111 1111 1");
+    });
 
-    it('deletes from middle of value', fakeAsync(() => {
-
-      inputEl.nativeElement.value = '4111 1111 111';
+    it("deletes from middle of value", async () => {
+      inputEl.nativeElement.value = "4111 1111 111";
       inputEl.nativeElement.selectionStart = 5;
       inputEl.nativeElement.selectionEnd = 5;
       inputEl.nativeElement.focus();
 
       const event = createKeyEvent(KEY_MAP.BACKSPACE);
 
-      inputEl.triggerEventHandler('keydown', event);
+      inputEl.triggerEventHandler("keydown", event);
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111 11');
+      expect(inputEl.nativeElement.value).toBe("4111 1111 11");
       expect(inputEl.nativeElement.selectionStart).toBe(3);
       expect(inputEl.nativeElement.selectionEnd).toBe(3);
-      expect(event.preventDefault).toBeCalled();
+      expect(event.preventDefault).toHaveBeenCalled();
+    });
 
-    }));
-
-    it('deletes from beginning of value', fakeAsync(() => {
-
-      inputEl.nativeElement.value = '5 411 1111';
+    it("deletes from beginning of value", async () => {
+      inputEl.nativeElement.value = "5 411 1111";
       inputEl.nativeElement.selectionStart = 2;
       inputEl.nativeElement.selectionEnd = 2;
       inputEl.nativeElement.focus();
 
       const event = createKeyEvent(KEY_MAP.BACKSPACE);
 
-      inputEl.triggerEventHandler('keydown', event);
+      inputEl.triggerEventHandler("keydown", event);
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 111');
+      expect(inputEl.nativeElement.value).toBe("4111 111");
       expect(inputEl.nativeElement.selectionStart).toBe(0);
       expect(inputEl.nativeElement.selectionEnd).toBe(0);
-      expect(event.preventDefault).toBeCalled();
+      expect(event.preventDefault).toHaveBeenCalled();
+    });
 
-    }));
-
-    it('does not modify deleting from end of value', fakeAsync(() => {
-
-      inputEl.nativeElement.value = '4111 1111 111';
+    it("does not modify deleting from end of value", async () => {
+      inputEl.nativeElement.value = "4111 1111 111";
       inputEl.nativeElement.selectionStart = 13;
       inputEl.nativeElement.selectionEnd = 13;
       inputEl.nativeElement.focus();
 
       const event = createKeyEvent(KEY_MAP.BACKSPACE);
-      inputEl.triggerEventHandler('keydown', event);
+      inputEl.triggerEventHandler("keydown", event);
       fixture.detectChanges();
-      tick(10);
-      expect(inputEl.nativeElement.value).toBe('4111 1111 111');
+      expect(inputEl.nativeElement.value).toBe("4111 1111 111");
       expect(inputEl.nativeElement.selectionStart).toBe(13);
       expect(inputEl.nativeElement.selectionEnd).toBe(13);
-      expect(event.preventDefault).not.toBeCalled();
-
-    }));
+      expect(event.preventDefault).not.toHaveBeenCalled();
+    });
   });
 
-  describe('exportAs cases', () => {
+  describe("exportAs cases", () => {
     @Component({
-      template: `<input type="tel" ccNumber #ccNumber="ccNumber">
-      <span class="scheme">{{ ccNumber.resolvedScheme$ | async }}</span>`,
+      template: `<input type="tel" ccNumber #ccNumber="ccNumber" />
+        <span class="scheme">{{ ccNumber.resolvedScheme$ | async }}</span>`,
       imports: [CreditCardFormatDirective, CommonModule],
     })
     class TestCreditCardFormatComponent {}
@@ -158,16 +151,18 @@ describe('Directive: CreditCardFormat', () => {
         imports: [TestCreditCardFormatComponent],
       });
       fixture = TestBed.createComponent(TestCreditCardFormatComponent);
-      inputEl = fixture.debugElement.query(By.css('input'));
+      inputEl = fixture.debugElement.query(By.css("input"));
     });
 
-    it('should provide resolved scheme via exportAs', () => {
-      (inputEl.nativeElement as HTMLInputElement).value = '4111111111111111';
-      inputEl.triggerEventHandler('input', null);
+    it("should provide resolved scheme via exportAs", () => {
+      (inputEl.nativeElement as HTMLInputElement).value = "4111111111111111";
+      inputEl.triggerEventHandler("input", null);
       fixture.detectChanges();
 
-      const span: HTMLSpanElement = fixture.debugElement.query(By.css('.scheme')).nativeElement;
-      expect(span.textContent).toBe('visa');
+      const span: HTMLSpanElement = fixture.debugElement.query(
+        By.css(".scheme"),
+      ).nativeElement;
+      expect(span.textContent).toBe("visa");
     });
   });
 });
